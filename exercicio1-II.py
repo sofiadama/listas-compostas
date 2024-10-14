c366e1cb9bffc683eb98f8e10faef01baea32b04
SOFIA <sofiadamasin@gmail.com>
2024-10-14 18:07:56 -0300

    Atualizar o exercicio1-II.py

--- exercicio1-II.py
+++ exercicio1-II.py
@@ -3,9 +3,10 @@ Q = int(input('Cidadãos: '))
 if Q < 4 and Q > 233000:
     print('Valor inválido!')
     Q = int(input('Digite novamente: '))
-
+   
 else:
-    Vi = [int(input('Digite 1 ou 0: '))][:Q]
+    Vi = list(map(int,input('Digite 1 ou 0: ').split()))[:Q]
+    
     S = 0
     N = 0
     for i in Vi:
@@ -13,6 +14,7 @@ else:
             S += 1
         elif i == 1:
             N += 1
+            
     if S > N:
         print(S)
     elif N > S:
